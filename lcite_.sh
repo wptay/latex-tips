f66be092a2f531ef5c9120adc95aa43fa0229b76
wptay <wptay@ntu.edu.sg>
2021-11-15 11:31:46 +0800

    lcite_.sh for Linux

---
+++ lcite_.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set +v
+
+# Change this accordingly
+export BIBINPUTS='.\bib'
+read -p 'Enter target: ' target
+lrefaux=lref-$target.aux
+lrefbbl=lref-$target.bbl
+echo $lrefaux, $lrefbbl
+read -p "Press enter to continue"
+
+latex $target
+bibtex lref.aux
+latex $target
+bibtex lref.aux
+cp lref.aux $lrefaux
+cp lref.bbl $lrefbbl
+latex $target
+latex $target
+latex $target
+pdflatex -synctex=-1 -interaction=nonstopmode $target
